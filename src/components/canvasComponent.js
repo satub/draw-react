9a153c4798d03f621f59d20b1b9b3fcbd6a6118d
satub <satu.barnhill@gmail.com>
2016-11-15 15:08:08 -0500

    mouseMove

--- src/components/canvasComponent.js
+++ src/components/canvasComponent.js
@@ -23,7 +23,7 @@ export default class CanvasComponent extends React.Component {
     let rendered = this.state.shapes.map((shape, i) => <DefaultShape xCo={shape[0]} yCo={shape[1]} key={i}/>)
     return (
       <div id="painting">
-        <canvas onClick={this.handleClick} id="ourCanvas" width="700" height="400" style={{backgroundColor: '#008811', borderRadius: 10, borderWidth: 0.5, borderColor: '#d6d7da', opacity: '0.75'}}>
+        <canvas onMouseMove={this.handleClick} id="ourCanvas" width="700" height="400" style={{backgroundColor: '#008811', borderRadius: 10, borderWidth: 0.5, borderColor: '#d6d7da', opacity: '0.75'}}>
           {rendered}
         </canvas>
       </div>
