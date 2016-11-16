4c2f7cd99030922240cc56cb41be85fdb510a846
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-11-16 17:42:47 -0500

    working version

--- server.js
+++ server.js
@@ -20,8 +20,12 @@ app.use(require('webpack-dev-middleware')(compiler, {
 var jsonParser = bodyParser.json();
 app.use(jsonParser)
 
-
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+// app.use(bodyParser.json({type: 'application/json'}));
 app.use(require('webpack-hot-middleware')(compiler));
+// app.use(favicon(__dirname + '/assets/public/favicon.ico'));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, './index.html'));
@@ -52,4 +56,56 @@ const io = require('socket.io')(server);
     io.emit('draw event', req.body.drawCoords);
  })
 
-/
+// "use strict";
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const twilio = require('twilio');
+ 
+// const app = express();
+ 
+// // Run server to listen on port 3000.
+// const server = app.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
+ 
+// const io = require('socket.io')(server);
+ 
+// app.use(bodyParser.urlencoded({ extended: false } ));
+// app.use(express.static('static'));
+ 
+// // Set socket.io listeners.
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+ 
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+ 
+// // Set Express routes.
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/views/index.html');
+// });
+ 
+// app.post('/events', (req, res) => {
+//   let to = req.body.To;
+//   let fromNumber = req.body.From;
+//   let callStatus = req.body.CallStatus;
+//   let callSid = req.body.CallSid;
+ 
+//   io.emit('call progress event', { to, fromNumber, callStatus, callSid });
+ 
+//   console.log(to, from, callStatus, callSid);
+//   res.send('Event received');
+// });
+ 
+// app.post('/voice', (req, res) => {
+//   // Generate a TwiML response
+//   let twiml = new twilio.TwimlResponse();
+//   // Talk in a robot voice over the phone.
+//   twiml.say('Call progress events are rad');
+//   // Set the response type as XML.
+//   res.header('Content-Type', 'text/xml');
+//   // Send the TwiML as the response.
+//   res.send(twiml.toString());
+// });
